9022363483180464cb28300d279711e2f6b6b574
mowsumi12 <164328415+mowsumi12@users.noreply.github.com>
2024-12-15 21:33:57 +0600

    Update prefix.js

--- TINA/commands/prefix.js
+++ TINA/commands/prefix.js
@@ -1,59 +1,59 @@
+const dipto = require('axios');
+const fs = require('fs-extra');
+const path = require('path');
+const moment = require('moment-timezone');
+const pathFile = __dirname + '/cache/d1p.txt';
+if (!fs.existsSync(pathFile))
+  fs.writeFileSync(pathFile, 'true');
+  const isEnable = fs.readFileSync(pathFile, 'utf-8');
 module.exports.config = {
-  name: "prefix",
-  version: "1.0.0",
-  hasPermssion: 0,
-  credits: "ALVI",
-  description: "Xem prefix của BOT",
-  commandCategory: "Dành cho Admin",
-  usages: "",
-  cooldowns: 5,
+name: "prefix",
+version: "1.0.0",
+hasPermssion: 0,
+credits: "nazrul",
+description: "guide",
+commandCategory: "system",
+usages: "",
+cooldowns: 5,
 };
-
-module.exports.handleEvent = async function ({ api, event }) {
-  const axios = require("axios");
-  const request = require("request");
-  const fs = require("fs");
-  const moment = require("moment-timezone");
-  var times = moment.tz("Asia/Ho_Chi_Minh").format("HH:mm:ss || D/MM/YYYY");
-  var thu = moment.tz("Asia/Ho_Chi_Minh").format("dddd");
-  moment.tz("Asia/Ho_Chi_Minh").format("dddd");
-  if (thu == "Sunday") thu = "𝚂𝚞𝚗𝚍𝚊𝚢";
-  if (thu == "Monday") thu = "𝙼𝚘𝚗𝚍𝚊𝚢";
-  if (thu == "Tuesday") thu = "𝚃𝚞𝚎𝚜𝚍𝚊𝚢";
-  if (thu == "Wednesday") thu = "𝚆𝚎𝚍𝚗𝚎𝚜𝚍𝚊𝚢";
-  if (thu == "Thursday") thu = "𝚃𝚑𝚞𝚛𝚜𝚍𝚊𝚢";
-  if (thu == "Friday") thu = "𝙵𝚛𝚒𝚍𝚊𝚢";
-  if (thu == "Saturday") thu = "𝚂𝚊𝚝𝚞𝚛𝚍𝚊𝚢";
-  var { threadID, messageID, body } = event,
-    { PREFIX } = global.config;
-  let threadSetting = global.data.threadData.get(threadID) || {};
-  let prefix = threadSetting.PREFIX || PREFIX;
-  const timeStart = Date.now();
-  const tdung = require("./../../ALVI/prefix.json");
-  var video = tdung[Math.floor(Math.random() * tdung.length)].trim();
-  function vtuanhihi(videoUrl, vtuandz, callback) {
-    request(videoUrl)
-      .pipe(fs.createWriteStream(__dirname + `/` + vtuandz))
-      .on("close", callback);
+module.exports.handleEvent = async ({ api, event }) => {
+  if (isEnable == "true"){
+  const dipto2 = event.body ? event.body.toLowerCase() : '';
+   // const GP = "•┄┅════❁🌺❁════┅┄•\n${GP}\n•┄┅════❁🌺❁════┅┄•\n\n"; 
+// ===== 𝐍𝐀𝐙𝐑𝐔𝐋 𝐁𝐎𝐓 ====="
+     let d1PInfo = await api.getThreadInfo(event.threadID);
+  let diptoName = d1PInfo.threadName;
+    var time = moment.tz("Asia/Dhaka").format("LLLL");
+  const text = `—»✨[ 𝐏𝐫𝐞𝐟𝐢𝐱 𝐄𝐯𝐞𝐧𝐭 ]✨«—\n𝐍𝐀𝐌𝐄➢𝐈𝐬𝐥𝐚𝐦𝐢𝐜𝐤 𝐂𝐡𝐚𝐭\n𝐑𝐎𝐁𝐎𝐓 𝐏𝐑𝐄𝐅𝐈𝐗 ➢ ｢ ${global.config.PREFIX} ｣\n𝐑𝐎𝐁𝐎𝐓 𝐂𝐌𝐃➢ ｢ ${client.commands.size} ｣\n𝐓𝐈𝐌𝐄 ➢${time}\n𝐆𝐑𝐎𝐔𝐏 𝐍𝐀𝐌𝐄\n${diptoName}\n𝐎𝐖𝐍𝐄𝐑➢ 𝐍𝐀𝐙𝐑𝐔𝐋\n𝐂𝐫𝐞𝐚𝐭𝐨𝐫 ━➢ 𝐈𝐬𝐥𝐦𝐚𝐢𝐜𝐤 𝐂𝐡𝐚𝐭`
+  //const text2 = text[Math.floor(Math.random() * text.length)];
+const imgur = ["https://i.imgur.com/eQ3zllm.jpeg","https://i.imgur.com/cLKLwSY.jpeg"]
+  const link = imgur[Math.floor(Math.random() * imgur.length)];
+  const res = await dipto.get(link, { responseType: 'arraybuffer' })
+const ex = path.extname(link);
+  const filename = __dirname + `/cache/Shaon${ex}`;
+  fs.writeFileSync(filename, Buffer.from(res.data, 'binary'));
+  if (dipto2.indexOf("prefix") ===0|| dipto2.indexOf("Prefix") ===0 )
+  {
+api.sendMessage({body:`${text}`,attachment: fs.createReadStream(filename)},event.threadID,() => fs.unlinkSync(filename),event.messageID)
   }
-  if (body.toLowerCase() == "Prefix" || body.toLowerCase() == "prefix") {
-    let callback = function () {
-      return api.sendMessage(
-        {
-          body: `========『 𝙿𝚁𝙴𝙵𝙸𝚇 』========\n▱▱▱▱▱▱▱▱▱▱▱▱▱\n\n╭───────•◈•────────╮\n             🄰🄻🅅🄸🄱🄾🅃       \n\n✿𝙶𝚛𝚘𝚞𝚙 𝙿𝚛𝚎𝚏𝚒𝚡: ${prefix}\n۞𝚂𝚢𝚜𝚝𝚎𝚖 𝙿𝚛𝚎𝚏𝚒𝚡 : ${global.config.PREFIX}\n✪𝙱𝚘𝚝 𝙽𝚊𝚖𝚎: ${global.config.BOTNAME}\n❁𝙱𝚘𝚝 𝙲𝚞𝚛𝚛𝚎𝚗𝚝𝚕𝚢 𝙷𝚊𝚜:${
-            client.commands.size
-          } 𝙲𝚘𝚖𝚖𝚊𝚗𝚍𝚜\n✴𝙿𝚒𝚗𝚐: ${Date.now() - timeStart}ms\n╰───────•◈•────────╯\n\n▱▱▱▱▱▱▱▱▱▱▱▱▱\n『  ${thu} || ${times} 』`,
-          attachment: [fs.createReadStream(__dirname + `/video.mp4`)],
-        },
-        event.threadID,
-        () => {
-          fs.unlinkSync(__dirname + `/video.mp4`);
-        },
-        event.messageID
-      );
-    };
-    vtuanhihi(video, "video.mp4", callback);
+ }
+}
+module.exports.run = async ({api,args, event}) => {
+try {
+  if (args[0] == 'on') {
+    fs.writeFileSync(pathFile, 'true');
+    api.sendMessage('no prefix on successfully', event.threadID, event.messageID);
   }
-};
+  else if (args[0] == 'off') {
+    fs.writeFileSync(pathFile, 'false');
+    api.sendMessage('no prefix off successfully', event.threadID, event.messageID);
+  }
+  else if (!args[0]){
+    api.sendMessage(`Wrong format ${this.config.name}use off/on`, event.threadID, event.messageID);
+  }
+  }
+  catch(e) {
+    console.log(e);
+  }
 
-module.exports.run = async ({ api, event, args, Threads }) => {};
+}

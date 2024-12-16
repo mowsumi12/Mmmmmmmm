105f8811fa52932d630af6920aa3c588ee893f44
mowsumi12 <164328415+mowsumi12@users.noreply.github.com>
2024-12-17 05:30:44 +0600

    Update and rename trans-hindi.js to Bhindi.js

--- TINA/commands/Bhindi.js
+++ TINA/commands/Bhindi.js
@@ -34,6 +34,6 @@ module.exports.run = async ({ api, event, args }) => {
 		var text = '';
 		retrieve[0].forEach(item => (item[0]) ? text += item[0] : '');
 		var fromLang = (retrieve[2] === retrieve[8][0][0]) ? retrieve[2] : retrieve[8][0][0]
-		api.sendMessage(` ${text}\n - 🍂🍂 ${fromLang} to 𝐇𝐢𝐧𝐝𝐢🍂🍂`, event.threadID, event.messageID);
+		api.sendMessage(` ${text}`, event.threadID, event.messageID);
 	});
                                     }

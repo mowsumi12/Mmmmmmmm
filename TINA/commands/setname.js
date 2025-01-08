ba5d33170c8eb4e2df04560c0345a897f999d147
mowsumi12 <164328415+mowsumi12@users.noreply.github.com>
2025-01-08 19:22:43 +0600

    Add files via upload

--- TINA/commands/setname.js
+++ TINA/commands/setname.js
@@ -1,10 +1,10 @@
 module.exports.config = {
 	name: "setname",
 	version: "1.0.0",
-	hasPermssion: 2,
-	credits: "nazrul",
-  description: "Change the nickname in your group or the person you tag",
-	commandCategory: "Group",
+	hasPermssion: 0,
+	credits: "Mirai Team",
+	description: "Đổi biệt danh trong nhóm của bạn hoặc của người bạn tag",
+	commandCategory: "other",
 	usages: "[name]",
 	cooldowns: 3
 };
@@ -14,4 +14,4 @@ module.exports.run = async function({ api, event, args }) {
 	const mention = Object.keys(event.mentions)[0];
 	if (!mention) return api.changeNickname(`${name}`, event.threadID, event.senderID);
 	if (mention[0]) return api.changeNickname(`${name.replace(event.mentions[mention], "")}`, event.threadID, mention);
-    }
+}
